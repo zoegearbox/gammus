ae6cf1962412fd8d570aabf00f93615552e8d11c
udincabbage <cumie.banzhar@gmail.com>
2018-11-29 21:57:45 +0800

    Revert "Merge branch 'master' of https://github.com/udincabbage/gammus"
    This reverts commit 5fc976d83580fcf76c920c43bfea6cb5ab69ff8f, reversing
    changes made to 8c1aca832225ae57a32bb9516507840f8bd9d18c.

--- includes/komoditas.inc.php
+++ includes/komoditas.inc.php
@@ -1,36 +1,36 @@
 <?php
 class Komoditas{
-
+	
 	private $conn;
 	private $table_name = "t_tahun_akademik";
-
+	
 	public $id;
-	public $ta;
-
+	public $ta; 
+	
 	public function __construct($db){
 		$this->conn = $db;
 	}
-
+	
 	function insert(){
-
-		$query = "insert into ".$this->table_name." (status,tahun_akademik)values('1',?)";
+		
+		$query = "insert into ".$this->table_name." values('','','','1',?)";
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(1, $this->ta); ;
-
+		
 		if($stmt->execute()){
 			return true;
 		}else{
 			return false;
 		}
-
+		
 	}
-
+	
 	function readAll(){
 
 		$query = "SELECT * FROM ".$this->table_name." ORDER BY updated_at ASC";
 		$stmt = $this->conn->prepare( $query );
 		$stmt->execute();
-
+		
 		return $stmt;
 	}
 	//test
@@ -39,13 +39,13 @@ class Komoditas{
 		$query = "SELECT SUM(harga) AS total FROM ".$this->table_name." ";
 		$stmt = $this->conn->prepare( $query );
 		$stmt->execute();
-
+		
 		return $stmt;
 	}
-
+	
 	// used when filling up the update product form
 	function readOne(){
-
+		
 		$query = "SELECT * FROM " . $this->table_name . " WHERE id=? LIMIT 0,1";
 
 		$stmt = $this->conn->prepare( $query );
@@ -53,26 +53,25 @@ class Komoditas{
 		$stmt->execute();
 
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
+		
 		$this->id = $row['id'];
-		$this->ta = $row['tahun_akademik'];
+		$this->ta = $row['tahun_akademik']; 
 	}
-
+	
 	// update the product
 	function update(){
 
-		$query = "UPDATE
-					" . $this->table_name . "
-				SET
+		$query = "UPDATE 
+					" . $this->table_name . " 
+				SET 
 					tahun_akademik = :ta
 				WHERE
 					id = :id";
 
 		$stmt = $this->conn->prepare($query);
 
-		$stmt->bindParam(':ta', $this->ta);
-		$stmt->bindParam(':id', $this->id);
-
+		$stmt->bindParam(':ta', $this->ta); 
+		
 		// execute the query
 		if($stmt->execute()){
 			return true;
@@ -80,12 +79,12 @@ class Komoditas{
 			return false;
 		}
 	}
-
+	
 	// delete the product
 	function delete(){
-
+	
 		$query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
-
+		
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(1, $this->id);
 
