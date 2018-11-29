125657b961588574d53f43880ff641938b2d50a7
udincabbage <cumie.banzhar@gmail.com>
2018-11-29 22:56:59 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/udincabbage/gammus""
    This reverts commit ae6cf1962412fd8d570aabf00f93615552e8d11c.

--- komoditas-ubah.php
+++ komoditas-ubah.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'header.php';
-include_once 'includes/nilai.inc.php';
-$pgn = new Nilai($db);
+// include_once 'includes/nilai.inc.php';
+// $pgn = new Nilai($db);
 
 $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
 
@@ -14,10 +14,9 @@ $eks->readOne();
 
 if($_POST){
 
-	$eks->kt = $_POST['kt'];
-	$eks->tp = $_POST['tp'];
-	$eks->jm = $_POST['jm'];
-	
+	$eks->id = $_POST['id'];
+	$eks->ta = $_POST['ta'];
+
 	if($eks->update()){
 		// echo "<script>location.href='komoditas.php'</script>";
 		echo "<meta http-equiv='refresh' content='2; url=komoditas.php'>";
@@ -26,7 +25,7 @@ if($_POST){
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
   <strong>Berhasil Ubah Data!</strong>.
 </div>
-		
+
 		<?php
 	} else{
 ?>
@@ -41,27 +40,28 @@ if($_POST){
 		<div class="row">
 		  <div class="col-xs-12 col-sm-12 col-md-8">
 		  	<div class="page-header">
-			  <h5>Ubah Komoditas</h5>
+			  <h5>Ubah Tahun</h5>
 			</div>
-			
+
 			    <form method="post">
+						<div class="form-group">
+					    <label for="tp">ID Tahun</label>
+							<input type="text" class="form-control" id="id" name="id" value="<?php echo $eks->id; ?>">
+
+					  </div>
 				  <div class="form-group">
-				    <label for="kt">Nama Komoditas</label>
-				    <input type="text" class="form-control" id="kt" name="kt" value="<?php echo $eks->kt; ?>">
+				    <label for="kt">Tahun Akademik</label>
+				    <input type="text" class="form-control" id="ta" name="ta" value="<?php echo $eks->ta; ?>">
 				  </div>
-				  <div class="form-group">
-				    <label for="tp">Harga Komoditas</label>
-				   
-		<input type="number" placeholder="10000" required class="form-control" id="tp" name="tp" min="1000" max="1000000" value="<?php echo $eks->tp; ?>">
-				  </div>
-				  
+
+
 				   <input type="text" class="form-control" id="jm" name="jm" value="<?php echo date("Y-m-d"); ?>"/>
-				   
-				   
+
+
 				  <button type="submit" class="btn btn-primary">Ubah</button>
 				  <button type="button" onclick="location.href='komoditas.php'" class="btn btn-success">Kembali</button>
 				</form>
-			  
+
 		  </div>
 		  <div class="col-xs-12 col-sm-12 col-md-4">
 		  	<?php include_once 'sidebar.php'; ?>
