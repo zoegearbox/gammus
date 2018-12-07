b3a86ad41fb48d6f883f7d69da321b42e8576894
udincabbage <cumie.banzhar@gmail.com>
2018-12-07 20:19:40 +0800

    Update nkit_sms_santri.sql
    DATABASE

--- nkit_sms_santri.sql
+++ nkit_sms_santri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2018 at 02:51 PM
+-- Generation Time: Dec 07, 2018 at 01:19 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -70,6 +70,15 @@ CREATE TABLE `inbox` (
   `RecipientID` text NOT NULL,
   `Processed` enum('false','true') NOT NULL DEFAULT 'false'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `inbox`
+--
+
+INSERT INTO `inbox` (`UpdatedInDB`, `ReceivingDateTime`, `Text`, `SenderNumber`, `Coding`, `UDH`, `SMSCNumber`, `Class`, `TextDecoded`, `ID`, `RecipientID`, `Processed`) VALUES
+('2018-11-30 03:25:38', '2018-11-30 02:25:36', '0049006E0066006F002000620061006700610069006D0061006E006100200063006100720061006E007900610020006D0065006E00670065007400610068007500690020006E0069007300200061006E0061006E006400610020007300610075006B0061006E0069003F0020', '+6282153278782', 'Default_No_Compression', '', '+6281107908', -1, 'Info bagaimana caranya mengetahui nis ananda saukani? ', 13, 'Prolink', 'true'),
+('2018-11-30 03:26:51', '2018-11-30 02:26:50', '0048006100660061006C0061006E002000310037003900320030003000300031', '+6282153278782', 'Default_No_Compression', '', '+6281107908', -1, 'Hafalan 17920001', 14, 'Prolink', 'true'),
+('2018-11-30 03:28:43', '2018-11-30 02:28:41', '00590061', '+6282153278782', 'Default_No_Compression', '', '+6281107908', -1, 'Ya', 15, 'Prolink', 'true');
 
 -- --------------------------------------------------------
 
@@ -182,7 +191,8 @@ CREATE TABLE `phones` (
 --
 
 INSERT INTO `phones` (`ID`, `UpdatedInDB`, `InsertIntoDB`, `TimeOut`, `Send`, `Receive`, `IMEI`, `Client`, `Battery`, `Signal`, `Sent`, `Received`) VALUES
-('Prolink', '2018-07-23 01:22:33', '2018-07-23 01:17:42', '2018-07-23 01:22:43', 'yes', 'yes', '353907046377130', 'Gammu 1.28.90, Windows Server 2007, GCC 4.4, MinGW 3.13', 100, 54, 1, 0);
+('Prolink', '2018-07-23 01:22:33', '2018-07-23 01:17:42', '2018-07-23 01:22:43', 'yes', 'yes', '353907046377130', 'Gammu 1.28.90, Windows Server 2007, GCC 4.4, MinGW 3.13', 100, 54, 1, 0),
+('Prolink', '2018-11-30 03:57:00', '2018-11-30 03:24:56', '2018-11-30 03:57:10', 'yes', 'yes', '350960684418347', 'Gammu 1.28.90, Windows Server 2007, GCC 4.4, MinGW 3.13', 0, 36, 4, 3);
 
 -- --------------------------------------------------------
 
@@ -212,6 +222,32 @@ CREATE TABLE `sentitems` (
   `CreatorID` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `sentitems`
+--
+
+INSERT INTO `sentitems` (`UpdatedInDB`, `InsertIntoDB`, `SendingDateTime`, `DeliveryDateTime`, `Text`, `DestinationNumber`, `Coding`, `UDH`, `SMSCNumber`, `Class`, `TextDecoded`, `ID`, `SenderID`, `SequencePosition`, `Status`, `StatusError`, `TPMR`, `RelativeValidity`, `CreatorID`) VALUES
+('2018-11-30 03:28:53', '0000-00-00 00:00:00', '2018-11-30 03:28:53', NULL, '004D00610061006600200070006500720069006E007400610068002000730061006C00610068002C002000670075006E0061006B0061006E003D00200048004100460041004C0041004E0028007300700061007300690029004E006F006D006F007200200049006E00640075006B002000530061006E0074007200690020006100740061007500200049004E0046004F0028007300700061007300690029006900730069002000700065007200740061006E007900610061006E002E', '+6282153278782', 'Default_No_Compression', '', '+62818445009', -1, 'Maaf perintah salah, gunakan= HAFALAN(spasi)Nomor Induk Santri atau INFO(spasi)isi pertanyaan.', 28, 'Prolink', 1, 'SendingOKNoReport', -1, 8, 255, 'nKIT'),
+('2018-11-30 03:27:18', '0000-00-00 00:00:00', '2018-11-30 03:27:18', NULL, '00530061006E0074007200690020004D007500680061006D006D006100640020005300610075006B0061006E006900200070006100640061002000620075006C0061006E002000300036002000740065006C006100680020006D0065006E0063006100700061006900200041006E002D0041006C0061007100200028003300300029002000640061006E00200073006500640061006E00670020006D0065006E00670068006100660061006C002000200041006E002D0049006B0068006C00610073002000280033003000290020', '+6282153278782', 'Default_No_Compression', '', '+62818445009', -1, 'Santri Muhammad Saukani pada bulan 06 telah mencapai An-Alaq (30) dan sedang menghafal  An-Ikhlas (30) ', 27, 'Prolink', 1, 'SendingOKNoReport', -1, 7, 255, 'nKIT'),
+('2018-11-30 03:25:43', '0000-00-00 00:00:00', '2018-11-30 03:25:43', NULL, '0054006500720069006D00610020004B0061007300690068002000740065006C006100680020006D0065006E006700670075006E0061006B0061006E0020006C006100790061006E0061006E0020006B0061006D0069002C0020006D006F0068006F006E002000740075006E006700670075002000310078003200340020006A0061006D0020006B0061006D006900200061006B0061006E0020006D0065006D00620061006C00610073002000700065007200740061006E007900610061006E00200041006E00640061002E', '+6282153278782', 'Default_No_Compression', '', '+62818445009', -1, 'Terima Kasih telah menggunakan layanan kami, mohon tunggu 1x24 jam kami akan membalas pertanyaan Anda.', 26, 'Prolink', 1, 'SendingOKNoReport', -1, 6, 255, 'nKIT'),
+('2018-11-30 03:25:06', '0000-00-00 00:00:00', '2018-11-30 03:25:06', NULL, '0070006500720063006F006200610061006E00200073006D0073002000670061007400650077006100790020006B006900720069006D00200073006D0073002000210021', '082153278782', 'Default_No_Compression', '', '+62818445009', -1, 'percobaan sms gateway kirim sms !!', 25, 'Prolink', 1, 'SendingOKNoReport', -1, 5, 255, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_broadcast`
+--
+
+CREATE TABLE `t_broadcast` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` int(11) DEFAULT '0',
+  `tahun` year(4) NOT NULL,
+  `bulan` char(2) NOT NULL,
+  `jumlah_kirim` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -232,10 +268,11 @@ CREATE TABLE `t_hafalan` (
 --
 
 INSERT INTO `t_hafalan` (`id`, `created_at`, `updated_at`, `status`, `juz`, `surah`) VALUES
-(1, '2018-10-30 22:31:46', '2018-10-30 22:31:46', 0, 30, 'An-nas'),
-(2, '2018-10-30 22:33:04', '2018-10-30 22:33:04', 0, 30, 'An-Alaq'),
+(1, '2018-10-30 22:31:46', '2018-11-30 06:06:30', 0, 30, 'An-Naas'),
+(2, '2018-10-30 22:33:04', '2018-12-01 00:53:35', 0, 30, 'Al-Alaq'),
 (3, '2018-10-30 22:33:04', '2018-10-30 22:33:04', 0, 30, 'An-Ikhlas'),
-(4, '2018-10-30 22:33:04', '2018-10-30 22:33:04', 0, 30, 'An-Lahab');
+(4, '2018-10-30 22:33:04', '2018-12-01 00:36:24', 0, 30, 'Al-Lahab'),
+(6, '2018-11-29 22:52:03', '2018-11-29 22:52:03', 1, 30, 'Ad-Dhuha');
 
 -- --------------------------------------------------------
 
@@ -255,6 +292,17 @@ CREATE TABLE `t_hafiz` (
   `penambahan_hafalan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_hafiz`
+--
+
+INSERT INTO `t_hafiz` (`id`, `created_at`, `updated_at`, `status`, `id_santri`, `tahun`, `bulan`, `pencapaian_hafalan`, `penambahan_hafalan`) VALUES
+(1, '2018-11-29 23:13:32', '2018-11-29 23:13:32', 1, 1, 2018, '06', 1, 2),
+(2, '2018-11-30 03:08:43', '2018-11-30 21:56:30', 1, 1, 2018, '10', 2, 3),
+(3, '2018-12-01 01:47:31', '2018-12-01 01:47:31', 1, 2, 2018, '10', 3, 3),
+(4, '2018-12-05 14:03:15', '2018-12-05 14:03:15', 1, 2, 2018, '12', 2, 4),
+(5, '2018-12-05 14:04:01', '2018-12-05 14:04:01', 1, 1, 2018, '12', 2, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -271,6 +319,14 @@ CREATE TABLE `t_kelas` (
   `id_tahun_akademik` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_kelas`
+--
+
+INSERT INTO `t_kelas` (`id`, `created_at`, `updated_at`, `status`, `nama_kelas`, `keterangan`, `id_tahun_akademik`) VALUES
+(1, '2018-11-29 22:10:00', '2018-11-30 22:18:19', 1, 'XIA', 'Kelas Tahfis A', 1),
+(2, '2018-11-29 22:10:34', '2018-11-29 22:10:34', 1, 'XIB', 'Kelas Tahfis B', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -285,6 +341,14 @@ CREATE TABLE `t_kelas_santri` (
   `id_santri` int(11) NOT NULL,
   `id_kelas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_kelas_santri`
+--
+
+INSERT INTO `t_kelas_santri` (`id`, `created_at`, `updated_at`, `status`, `id_santri`, `id_kelas`) VALUES
+(1, '2018-11-29 22:38:23', '2018-12-01 01:41:32', 1, 1, 2),
+(2, '2018-12-01 01:35:49', '2018-12-01 01:36:26', 1, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -316,8 +380,16 @@ CREATE TABLE `t_santri` (
   `nis` char(12) NOT NULL,
   `nama_santri` varchar(164) NOT NULL,
   `tahun_akademik` year(4) NOT NULL,
-  `id_kelas` int(11) NOT NULL
+  `status_santri` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_santri`
+--
+
+INSERT INTO `t_santri` (`id`, `created_at`, `updated_at`, `status`, `nis`, `nama_santri`, `tahun_akademik`, `status_santri`) VALUES
+(1, '2018-11-29 22:37:12', '2018-12-01 00:34:26', 1, '17920001', 'Muhammad Saukani', 2017, 'Aktif'),
+(2, '2018-12-01 00:23:08', '2018-12-01 00:36:02', 1, '17920002', 'Mirza Yogy Kurniawan', 2018, 'Aktif');
 
 -- --------------------------------------------------------
 
@@ -338,7 +410,32 @@ CREATE TABLE `t_tahun_akademik` (
 --
 
 INSERT INTO `t_tahun_akademik` (`id`, `created_at`, `updated_at`, `status`, `tahun_akademik`) VALUES
-(0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, '2018');
+(1, '0000-00-00 00:00:00', '2018-11-29 22:50:55', 1, '2018'),
+(2, '0000-00-00 00:00:00', '2018-11-30 22:18:06', 1, '2019');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_wali`
+--
+
+CREATE TABLE `t_wali` (
+  `id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` int(11) DEFAULT '0',
+  `id_santri` int(11) NOT NULL,
+  `nama_wali` varchar(32) NOT NULL,
+  `no_hp` char(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_wali`
+--
+
+INSERT INTO `t_wali` (`id`, `created_at`, `updated_at`, `status`, `id_santri`, `nama_wali`, `no_hp`) VALUES
+(1, '2018-12-05 11:11:10', '2018-12-05 11:11:10', 1, 2, 'Marzuki Ali', '08115131846'),
+(2, '2018-12-05 12:19:35', '2018-12-05 12:19:35', 1, 1, 'Almarhum', '082153278782');
 
 --
 -- Indexes for dumped tables
@@ -437,8 +534,7 @@ ALTER TABLE `t_pesan`
 -- Indexes for table `t_santri`
 --
 ALTER TABLE `t_santri`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_kelas` (`id_kelas`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `t_tahun_akademik`
@@ -448,6 +544,14 @@ ALTER TABLE `t_tahun_akademik`
   ADD KEY `id` (`id`);
 
 --
+-- Indexes for table `t_wali`
+--
+ALTER TABLE `t_wali`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_santri` (`id_santri`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -455,13 +559,13 @@ ALTER TABLE `t_tahun_akademik`
 -- AUTO_INCREMENT for table `inbox`
 --
 ALTER TABLE `inbox`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `outbox`
 --
 ALTER TABLE `outbox`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `pbk`
@@ -485,25 +589,43 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT for table `t_hafalan`
 --
 ALTER TABLE `t_hafalan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `t_hafiz`
 --
 ALTER TABLE `t_hafiz`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `t_kelas`
 --
 ALTER TABLE `t_kelas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `t_kelas_santri`
+--
+ALTER TABLE `t_kelas_santri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `t_santri`
 --
 ALTER TABLE `t_santri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `t_tahun_akademik`
+--
+ALTER TABLE `t_tahun_akademik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `t_wali`
+--
+ALTER TABLE `t_wali`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
